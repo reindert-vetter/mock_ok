96dca808e0810fa5f6f34c0e5a81022ebc571940
Reindert Vetter <reindert@myparcel.nl>
2018-11-28 01:00:16 +0100

    4

---
+++ src/app/Domains/Collect/Controllers/CollectorController.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Domains\Collect\Controllers;
+
+use App\Domains\Collect\Models\PreserveRequest;
+use App\Domains\Collect\Models\PreserveResult;
+use App\Domains\Collect\Providers\RequestProvider;
+use Illuminate\Http\Request as ConsumerRequest;
+use Illuminate\Http\Response as ConsumerResponse;
+
+/**
+ * @author Reindert Vetter
+ */
+class CollectorController
+{
+    /**
+     * @param ConsumerRequest $consumerRequest
+     * @param RequestProvider $requestProvider
+     * @return ConsumerResponse
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function handle(ConsumerRequest $consumerRequest, RequestProvider $requestProvider)
+    {
+        PreserveRequest::all();
+        $request = new PreserveRequest([
+            'method'  => $consumerRequest->method(),
+            'uri'     => $consumerRequest->getUri(),
+            'query'   => $consumerRequest->query->all(),
+            'body'    => $consumerRequest->getContent(),
+            'headers' => $consumerRequest->headers->all(),
+        ]);
+        $clientResponse = $requestProvider->handle($request);
+
+        $response = new PreserveResult([
+            'body'    => (string) $clientResponse->getBody(),
+            'status'  => $clientResponse->getStatusCode(),
+            'headers' => $clientResponse->getHeaders(),
+            'request' => $request,
+        ]);
+
+        $response->save();
+        
+        return new ConsumerResponse(
+            $clientResponse->getBody(),
+            $clientResponse->getStatusCode(),
+            $clientResponse->getHeaders()
+        );
+    }
+}
---
+++ src/app/Domains/Collect/Providers/RequestProvider.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Domains\Collect\Providers;
+
+
+use App\Domains\Collect\Models\PreserveRequest;
+use GuzzleHttp\Client;
+use GuzzleHttp\RequestOptions;
+
+/**
+ * @author Reindert Vetter
+ */
+class RequestProvider
+{
+    /**
+     * @param PreserveRequest $request
+     * @return mixed|\Psr\Http\Message\ResponseInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function handle(PreserveRequest $request): \Psr\Http\Message\ResponseInterface
+    {
+        $client = new Client();
+        $options = [
+            'headers' => $request->headers,
+            'query' => $request->query,
+            'timeout' => 3,
+            RequestOptions::SYNCHRONOUS => true,
+        ];
+
+        $result = $client->request($request->method, $request->uri, $options);
+
+        return $result;
+    }
+}
