2029ad69afbb1a53d9318b376011c0556dec2fca
Reindert Vetter <reindert@myparcel.nl>
2019-05-23 13:46:50 +0200

    check regex strict

--- src/resources/views/body-template.blade.php
+++ src/resources/views/body-template.blade.php
@@ -7,8 +7,8 @@ return [
     'when' => function (Request $request): bool {
         return
             $request->isMethod('{{ $method }}') &&
-            preg_match('#^{!! $url !!}$#', $request->fullUrl())@if($method !== 'GET') &&
-            preg_match('#^{!! $requestBody !!}$#', (string) $request->getContent())@endif;
+            false !== preg_match('#^{!! $url !!}$#', $request->fullUrl())@if($method !== 'GET') &&
+            false !== preg_match('#^{!! $requestBody !!}$#', (string) $request->getContent())@endif;
     },
 
     'response' => [
